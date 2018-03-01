79518e60800c0d9b776cd6e10ad54c25dfa30335
vaishalivsb <31504057+vaishalivsb@users.noreply.github.com>
2018-03-01 10:26:55 +0000

    Create koddno.c

---
+++ koddno.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int N,K,a,i,c=0;
+	scanf("%d%d",&N,&K);
+	for(i=1;i<=N;i++)
+	{
+		scanf("%d",&a);
+		if(a%2!=0)
+		{
+			c++;
+		}
+		if(c==2)
+		{
+			printf("%d",a);
+		}
+	}
+	return 0;
+}
